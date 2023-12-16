c5e7d2dbcd1b8a0a932f97693829a60102e0f7c6
FRdzanek <146526187+FRdzanek@users.noreply.github.com>
2023-12-16 14:02:30 +0100

    Drugi commit

---
+++ ShiftPlaner/viewer/forms.py
@@ -0,0 +1,98 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db.models import Sum
+from django.core.exceptions import ValidationError
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from .constants import SHIFT_CHOICES
+from .models import Schedule, UserAvailability
+from django.forms.widgets import DateInput
+
+class RegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+class ScheduleForm(forms.ModelForm):
+    class Meta:
+        model = Schedule
+        fields = ['user', 'work_date']
+
+    work_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super(ScheduleForm, self).__init__(*args, **kwargs)
+
+        if user and not user.is_staff:
+            self.fields['work_date'].widget = DateInput(attrs={'type': 'date'})
+
+
+            del self.fields['user']
+
+
+            del self.fields['UniqueID']
+
+
+
+
+    def save(self, commit=True):
+        # Ustaw pole user na None dla zwykłego użytkownika
+        if not self.instance.user.is_staff:
+            self.instance.user = None
+
+        return super(ScheduleForm, self).save(commit)
+
+
+
+class UserAvailabilityForm(forms.ModelForm):
+    class Meta:
+        model = UserAvailability
+        fields = ['user_id', 'day', 'shift_preferences']
+
+    shift_preferences = forms.ChoiceField(choices=SHIFT_CHOICES)
+    day = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+
+    def __init__(self, *args, user=None, **kwargs):
+        super(UserAvailabilityForm, self).__init__(*args, **kwargs)
+
+        # Ustaw początkowe dane dla pola 'user_id'
+        if user:
+            self.fields['user_id'] = forms.ModelChoiceField(queryset=User.objects.filter(pk=user.pk), initial=user)
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+        user = cleaned_data.get('user_id')
+        day = cleaned_data.get('day')
+
+        # Sprawdź, czy istnieje już dyspozycja dla danego użytkownika na ten dzień
+        existing_availability = UserAvailability.objects.filter(user_id=user, day=day).exists()
+
+        if existing_availability:
+            raise ValidationError('Dyspozycja dla tego użytkownika na ten dzień już istnieje.')
+
+        return cleaned_data
+
+class AvailabilitySelectionForm(forms.ModelForm):
+    class Meta:
+        model = UserAvailability
+        fields = ['day', 'shift_preferences']
+
+class MonthlyScheduleForm(forms.Form):
+    MONTH_CHOICES = [
+        (1, 'January'),
+        (2, 'February'),
+        # Dodaj pozostałe miesiące
+    ]
+
+    month = forms.ChoiceField(choices=MONTH_CHOICES, label='Month')
+    year = forms.IntegerField(label='Year')
---
+++ ShiftPlaner/viewer/views.py
@@ -0,0 +1,276 @@
+from datetime import timedelta, datetime
+from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.utils.timezone import make_aware
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle
+from . import models
+from .forms import UserAvailabilityForm
+from .models import UserAvailability, Shift, Schedule, WorkRestrictions
+from django.db.models import Sum
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('base')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('base')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('base')
+
+
+def base(request):
+    return render(request, "base.html")
+
+def is_admin(user):
+    return user.is_authenticated and user.is_staff
+
+def enter_availability(request):
+    if request.method == 'POST':
+        form = UserAvailabilityForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            instance = form.save(commit=False)
+            instance.user = user
+            instance.save()
+            return redirect('availability_list')
+    else:
+        form = UserAvailabilityForm()
+    return render(request, 'schedule/enter_availability.html', {'form': form})
+
+
+
+def root(request):
+    return render(request, 'root.html')
+
+
+
+
+def availability_list(request):
+    if request.user.is_staff:
+        # Administrator widzi wszystkie dyspozycje użytkowników
+        user_availabilities = UserAvailability.objects.all()
+    else:
+        # Zwykły użytkownik widzi tylko swoje dyspozycje
+        user_availabilities = UserAvailability.objects.filter(user_id=request.user)
+
+    context = {
+        'user_availabilities': user_availabilities,
+    }
+
+    return render(request, 'schedule/availability_list.html', context)
+
+@user_passes_test(is_admin, login_url='login')
+@login_required(login_url='login')
+def schedule_list(request):
+
+
+    # Przykład:
+    schedule = Schedule.objects.all()
+
+    return render(request, "schedule/schedule_list.html", {'schedule': schedule})
+
+    if created:
+        messages.info(request, "Work restrictions created successfully.")
+
+
+    shifts_data = [
+        {'shift_name': 'First_Shift', 'hours': 8, 'min_num_workers': 2, 'max_num_workers': 3},
+        {'shift_name': 'Second_Shift', 'hours': 8, 'min_num_workers': 2, 'max_num_workers': 3},
+        # Dodaj inne zmienne zmienne według potrzeb
+    ]
+
+    for shift_data in shifts_data:
+        shift, created = Shift.objects.get_or_create(
+            shift_name=shift_data['shift_name'],
+            defaults={
+                'hours': shift_data['hours'],
+                'min_num_workers': shift_data['min_num_workers'],
+                'max_num_workers': shift_data['max_num_workers']
+            }
+        )
+
+        if created:
+            messages.info(request, f"Shift {shift.shift_name} created successfully.")
+
+
+    users_availabilities = UserAvailability.objects.all()
+
+    schedule_entries = []
+
+    for user_availability in users_availabilities:
+        try:
+            shift = Shift.objects.get(shift_name=user_availability.shift_preferences)
+        except Shift.DoesNotExist:
+            messages.error(request, f"Shift {user_availability.shift_preferences} does not exist.")
+            continue
+
+        existing_hours_for_user = Schedule.objects.filter(
+            user=user_availability.user_id,
+            work_date=user_availability.day
+        ).aggregate(models.Sum('shift_id__hours'))['shift_id__hours__sum'] or 0
+
+        if existing_hours_for_user + shift.hours <= work_restrictions.max_daily_hours:
+            min_hours_between_shifts = work_restrictions.min_hours_between
+
+            if Schedule.objects.filter(
+                    user=user_availability.user_id,
+                    work_date__lt=user_availability.day,
+                    work_date__gte=user_availability.day - timedelta(hours=min_hours_between_shifts)
+            ).exists():
+                continue
+
+            schedule_entry = Schedule.objects.create(
+                user=user_availability.user_id,
+                shift_id=shift,
+                work_date=user_availability.day
+            )
+            schedule_entries.append(schedule_entry)
+
+    all_schedule_entries = Schedule.objects.all()
+
+    return render(request, 'schedule/schedule_list.html', {'schedule_entries': all_schedule_entries})
+
+
+
+@user_passes_test(is_admin, login_url='login')
+@login_required(login_url='login')
+def generate_schedule(request):
+
+    work_restrictions, created = WorkRestrictions.objects.get_or_create(
+        max_daily_hours=8,
+        min_hours_between=12,
+        hours_limit=8
+    )
+
+    if created:
+        messages.info(request, "Work restrictions created successfully.")
+
+
+    shifts_data = [
+        {'shift_name': 'First_Shift', 'hours': 8, 'min_num_workers': 2, 'max_num_workers': 3},
+        {'shift_name': 'Second_Shift', 'hours': 8, 'min_num_workers': 2, 'max_num_workers': 3},
+
+    ]
+
+    for shift_data in shifts_data:
+        shift, created = Shift.objects.get_or_create(
+            shift_name=shift_data['shift_name'],
+            defaults={
+                'hours': shift_data['hours'],
+                'min_num_workers': shift_data['min_num_workers'],
+                'max_num_workers': shift_data['max_num_workers']
+            }
+        )
+
+        if created:
+            messages.info(request, f"Shift {shift.shift_name} created successfully.")
+
+
+    users_availabilities = UserAvailability.objects.all()
+
+    schedule_entries = []
+
+    for user_availability in users_availabilities:
+        try:
+            shift = Shift.objects.get(shift_name=user_availability.shift_preferences)
+        except Shift.DoesNotExist:
+            messages.error(request, f"Shift {user_availability.shift_preferences} does not exist.")
+            continue
+
+        existing_hours_for_user = Schedule.objects.filter(
+            user=user_availability.user_id,
+            work_date=user_availability.day
+        ).aggregate(models.Sum('shift_id__hours'))['shift_id__hours__sum'] or 0
+
+        if existing_hours_for_user + shift.hours <= work_restrictions.max_daily_hours:
+            min_hours_between_shifts = work_restrictions.min_hours_between
+
+            if Schedule.objects.filter(
+                    user=user_availability.user_id,
+                    work_date__lt=user_availability.day,
+                    work_date__gte=user_availability.day - timedelta(hours=min_hours_between_shifts)
+            ).exists():
+                continue
+
+            schedule_entry = Schedule.objects.create(
+                user=user_availability.user_id,
+                shift_id=shift,
+                work_date=user_availability.day
+            )
+            schedule_entries.append(schedule_entry)
+
+    all_schedule_entries = Schedule.objects.all().order_by('work_date')
+
+    return render(request, 'schedule/schedule_list.html', {'schedule_entries': all_schedule_entries})
+
+@user_passes_test(is_admin, login_url='login')
+@login_required(login_url='login')
+def generate_pdf(request):
+    # Pobierz posortowane wpisy według daty
+    schedule_entries = Schedule.objects.order_by('work_date')
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; filename="schedule.pdf"'
+
+    doc = SimpleDocTemplate(response, pagesize=letter)
+    styles = getSampleStyleSheet()
+
+    data = [['User', 'Shift', 'Work Date']]
+
+    for schedule_entry in schedule_entries:
+        user = schedule_entry.user.username
+        shift = schedule_entry.shift_id.shift_name
+
+
+        work_date = make_aware(datetime.combine(schedule_entry.work_date, datetime.min.time()))
+
+
+        work_date = timezone.localtime(work_date).strftime('%Y-%m-%d')
+        data.append([user, shift, work_date])
+
+
+    table = Table(data)
+
+
+    style = TableStyle([('BACKGROUND', (0, 0), (-1, 0), '#77aaff'),
+                        ('TEXTCOLOR', (0, 0), (-1, 0), styles['Heading1'].textColor),
+                        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                        ('BACKGROUND', (0, 1), (-1, -1), '#eeeeee'),
+                        ('GRID', (0, 0), (-1, -1), 1, '#ffffff')])
+
+    table.setStyle(style)
+
+    content = [Paragraph("Schedule List", styles['Title']), table]
+
+    doc.build(content)
+
+    return response
